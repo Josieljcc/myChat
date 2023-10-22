8e6e2f9567be81d74f7f7818366b012cbf06098c
josiel-eway <josiel.jcc@hotmail.com>
2023-10-22 15:04:59 -0300

    chore: configure socketIo

--- types.ts
+++ types.ts
@@ -1,5 +1,16 @@
+import { Server as NetServer, Socket } from "net";
 import { Member, Profile, Server } from "@prisma/client";
+import { Server as SocketIOServer } from "socket.io";
+import { NextApiResponse } from "next";
 
 export type serverWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
 };
