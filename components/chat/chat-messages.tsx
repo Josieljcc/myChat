44ee80578113170cadd10635e5e6103464360202
josiel-eway <josiel.jcc@hotmail.com>
2023-10-29 18:30:38 -0300

    feat: implement useChatSocket to update messages in real time

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "dd MMM yyyy HH:mm";
 
@@ -40,6 +41,8 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const updateKey = `chat:${chatId}:messages:update`;
+  const addKey = `chat:${chatId}:messages`;
   const { data, fetchNextPage, hasNextPage, status, isFetchingNextPage } =
     useChatQuery({
       queryKey,
@@ -47,6 +50,7 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
+  useChatSocket({ updateKey, addKey, queryKey });
 
   if (status === "pending") {
     return (
